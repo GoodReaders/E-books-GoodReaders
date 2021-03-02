cb0148c1cf60ab37897516a97f102396051338a5
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-03-02 14:59:39 +0200

    c1

---
+++ js/MyBooks.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// cart constructor function
+const Cart = function (books) {
+    this.books = books;
+}
+
+// declare a cart variable
+let cart;
+
+// Add book to the cart
+Cart.prototype.addBook = function(bookName, bookImg, bookPrice) {
+    let newBook = new CartBook(bookName, bookImg, bookPrice);
+    this.books.push(newBook)
+}
+
+// remove book from the cart
+Cart.prototype.removeBook = function (bookName) {
+    let bookIndex;
+    for(let i=0; i<this.books.length; i++) {
+      if(this.books[i].bookName == bookName) {
+        bookIndex = i;
+        break;
+      }
+    }
+    this.books.splice(bookIndex, 1);
+}
+
+// save carts' books in the local storage
+Cart.prototype.saveToLocalStorage = function () {
+    localStorage.setItem('cart',JSON.stringify(this.books))
+}
+
+// constructor function for the cart book
+const CartBook = function(bookName, bookImg, bookPrice) {
+    this.bookName = bookName;
+    this.bookImg = bookImg;
+    this.bookPrice = bookPrice;
+}
+
+
+// Handel the event of clicking on remove link
+let divContainer = document.getElementById("main-div");
+divContainer.addEventListener('click', removeBookFromCart);
+
+function removeBookFromCart(event){
+    event.preventDefault();
+    let itemDom = document.querySelectorAll("item");
+
+    if(event.target.nodeName ==='A'){
+        event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
+       // cart.removeBook(/*book name*/)
+    }
+    cart.saveToLocalStorage();
+    renderCart();
+}
+
+
+
+// render all books from the cart onto the page
+function renderCart(){
+    loadCart();
+    clearCart();
+    showCart();
+}
+
+function loadCart(){
+    const cartBooks = JSON.parse(localStorage.getItem('cart')) || [];
+    cart = new Cart(cartBooks);
+}
+
+function clearCart() {
+    let subContainerDiv = document.getElementsByClassName("sub-container");
+    subContainerDiv.textContent = "";                               
+}
+
+function showCart(){
+    let subContainerDiv = document.getElementsByClassName('sub-container');
+    for(let i=0; i<cart.books.length; i++) {
+        /*code to render the cart*/
+    }
+}
+
+function calcTotalPrice() {
+   let total = 0;
+   for(let i=0; i<this.books.length; i++) {
+       total += this.books[i].bookPrice;
+   }
+   return total;
+}
