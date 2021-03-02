584431f6c4bc41927e454d23d251e4fc5d5f7aa5
Muhannad <Muhannadalmughrabi233@gmail.com>
2021-03-02 07:49:39 +0200

    finishes rendering still needs some fixes regarding the heading

--- js/Browse.js
+++ js/Browse.js
@@ -1,86 +1,94 @@
 let productDom = document.querySelector(".books");
+console.log(productDom);
 
 function Products() {
 }
 
-Products.prototype.getProducts = async () => {
+Products.prototype.getandRenderProducts = async () => {
     try {
         let result = await fetch("../../books.json");
         let data = await result.json();
         let products = data.books;
-        products = products[0]['self-help'];
-        products = products.map( book => {
-            const img = book['img-url'];
-            const name = book.name;
-            const author = book.author;
-            const price = book.price;
-            const publishedDate = book['published-date']
-            const pdf = book['book-url'];
-            const intro = book.introduction;
-            // const image = book.image-url;
-            return {img,name,author,price,publishedDate,pdf,intro};
-        })
-        console.log(products);
-        return products;
+        let categoriesArr=["self-help", "cookbooks", "poetry", "fitness", "Novel", "shortStories", "science", "Art"];
+        // let mytest = products[0];
+        // console.log(categoriesArr[1]);
+        let Allproducts = [];
+        let Allnewproducts =[];
+        for (let i =0;i<categoriesArr.length;i++) {
+            Allproducts = products[0][categoriesArr[i]];
+                Allnewproducts = Allproducts.map( book => {
+                const img = book['img-url'];
+                const name = book.name;
+                const author = book.author;
+                const price = book.price;
+                const publishedDate = book['published-date']
+                const pdf = book['book-url'];
+                const intro = book.introduction;
+                return {img,name,author,price,publishedDate,pdf,intro};
+             
+                
+            })
+            console.log(Allnewproducts)
+            let result = "";
+            Allnewproducts.forEach( product => {
+                result += `
+                <article class="one-book">
+                <div class="flip-card">
+                <div class="flip-card-inner">
+                <div class="flip-card-front">
+                <img src=${product.img}
+                alt="Avatar" >
+                </div>
+                <div class="flip-card-back">
+                <p>
+                ${product.intro} 
+                </p> 
+                </div>
+                </div>
+                </div>
+                <div class="book-Info">
+                <h6>${product.name}</h6>
+                <h6>By ${product.author}</h6>
+                <h6>${product.price}</h6>
+                <h6>${product.publishedDate}</h6>
+                <button class="contains-pdf">
+                <a href=${product.pdf} class="button">Read as soft copy <i
+                class="fas fa-book-reader"></i></a>
+                </button>
+                <button class="add-button">Add To My Library</button>
+                </div>
+                </article>
+                `;
+                
+            }
+                )
+                // let newheading = document.createElement('h2');
+                // productDom.appendChild(newheading);
+                // newheading.innerHTML = categoriesArr[i];
+                result = result + `<h2> ${categoriesArr[i]} <h2> `;
+                result+=`<div><div>`
+                console.log(result);
+                productDom.innerHTML+= result;
+
+        }
+
     } catch (error) {
         console.log(error)
     }
 }
-    // display product
-    function UI() {
-    
-    }
 
 
-UI.prototype.displayProducts = function(products) {
-    let result = "";
-    products.forEach( product => {
-        result += `
-        <article class="one-book">
-        <div class="flip-card">
-        <div class="flip-card-inner">
-        <div class="flip-card-front">
-        <img src=${product.img}
-        alt="Avatar" >
-        </div>
-        <div class="flip-card-back">
-        <p>
-        ${product.intro} 
-        </p> 
-        </div>
-        </div>
-        </div>
-        <div class="book-Info">
-        <h6>${product.name}</h6>
-        <h6>By ${product.author}</h6>
-        <h6>${product.price}</h6>
-        <h6>${product.publishedDate}</h6>
-        <button class="contains-pdf">
-        <a href=${product.pdf} class="button">Read as soft copy <i
-        class="fas fa-book-reader"></i></a>
-        </button>
-        <button class="add-button">Add To My Library</button>
-        </div>
-        </article>
-        `;
-    }); 
-
-    productDom.innerHTML = result;
-}
 
 document.addEventListener("DOMContentLoaded", function()
 {
-    const ui = new UI();
     const products = new Products();
-    // const storage = new Storage();
-    // // get all Products
-    products.getProducts().then(function(products) {
-        ui.displayProducts(products);
+    products.getandRenderProducts();
+    // .then(function(products) {
         // storage.saveProducts(products);
   
     // })
     // then( () => {
     //     ui.getButtons();
-    });
+    // });
      
 } );
